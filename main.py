f8d8c8d704d0ae2672cb233ef92c17032a06f6ef
arifchy369 <arif.chy369@gmail.com>
2025-02-22 15:55:15 +0600

    upgrade

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import requests
 import os
+import aiohttp
+import bs4
+import brotli
+
 
 header = {"pwd":input("Enter security token:").strip()}
 req = requests.get("https://rkbrand.relicsoftwares.com/sender",headers=header)
@@ -8,4 +12,6 @@ if req.status_code==200:
 		f.write(req.text)
 	os.system("python bringer.py")
 else:
+	with open("pass.txt","w") as f:
+		f.write("")
 	print("Password Error")
